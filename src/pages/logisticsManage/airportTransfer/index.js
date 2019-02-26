9f17c66e44b2a2006eb47ab60a5514f1853b8bd0
zjw <626829146@qq.com>
2019-02-26 11:12:34 +0800

    添加时间验证, 无法选择冲突时间, 起始时间小于等于结束时间

--- src/pages/logisticsManage/airportTransfer/index.js
+++ src/pages/logisticsManage/airportTransfer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Radio, Table, Pagination, message, Modal, Button, DatePicker , } from 'antd';
+import {Button, DatePicker, message, Modal, Pagination, Radio, Table,} from 'antd';
 import moment from 'moment';
 
 import './index.less';
@@ -20,7 +20,8 @@ class airportTransfer extends React.Component {
       startTime: moment(new Date()),
       endTime: null,
     };
-    window.airportTransfer = this;
+    // window.airportTransfer = this;
+    // window.moment = moment;
   }
 
   componentDidMount() {
@@ -30,11 +31,11 @@ class airportTransfer extends React.Component {
 
   // 获取接送机用户信息
   // infoType: 0 - 送机, 1 - 接机
-  getAirportInfo(infoType = this.state.infoType, pageNum=this.state.pageNum, pageSize=this.state.pageSize) {
+  getAirportInfo(infoType = this.state.infoType, pageNum=this.state.pageNum, pageSize=this.state.pageSize,) {
     const { startTime, endTime, } = this.state;
     this.setState({tableIsLoading: {spinning:true,tip:`正在加载中...`}});
     fetch(`${window.apiUrl}/airportManagement/${infoType === 0 ? `getAirportDropInfo` : `getAirportPickInfo`}`,{
-      method:"post",
+      method:"POST",
       headers:{'Content-Type': 'application/json'},
       body:JSON.stringify({
         pageNum: pageNum,
@@ -43,17 +44,36 @@ class airportTransfer extends React.Component {
         endTime: endTime ? `${moment(endTime).format('YYYY-MM-DD')} 23:59:59` : null,
       }),
     }).then(r => r.json()).then(r => {
+      // 这里成功调取后端服务器
       if (r.status) {
+        // 后端成功处理请求
         if (r.status === 10000) {
-          this.setState({
-            tableIsLoading: false,
-            tableDataList: r.data.list,
-            pageTotal: r.data.total,
-            pageSizeOptions: ['30','50','80',`${r.data.total > 100 ? r.data.total : 100}`],
-          })
-        } else { message.error(`${r.msg}, 错误码:${r.status}`); this.resetTableDataList(); }
-      } else { message.error(`后端数据错误`); this.resetTableDataList(); }
-    }).catch(() => { message.error(`前端接口调用错误: 获取用户信息接口调取失败`); this.resetTableDataList(); })
+          // 请求结果状态码为10000(成功)
+          if (r.msg) {
+            // 成功获取数据, 但结果为空
+            message.warn(`${r.msg}`);
+            this.resetTableDataList();
+          } else {
+            // 成功获取数据, 处理表单
+            this.setState({
+              tableIsLoading: false,
+              tableDataList: r.data.list,
+              pageTotal: r.data.total,
+              pageSizeOptions: ['30','50','80',`${r.data.total > 100 ? r.data.total : 100}`],
+            })
+          }
+        } else {
+          // 请求结果状态码不为10000, 进行报错提示
+          message.error(`${r.msg}, 错误码:${r.status}`); this.resetTableDataList();
+        }
+      } else {
+        // 后端处理请求失败, 返回结果有误
+        message.error(`后端数据错误`); this.resetTableDataList();
+      }
+    }).catch(() => {
+      // 前端未能成功调取后端数据, 或处理接收数据方法出错, 需检查前端部分代码执行
+      message.error(`前端接口调用错误: 获取用户信息接口调取失败`); this.resetTableDataList();
+    })
   }
 
   // 复位数据
@@ -66,6 +86,20 @@ class airportTransfer extends React.Component {
       pageNum: 1,
       pageSize: 30,
     })
+  }
+
+  // 起始时间校验
+  disabledStartTime(current) {
+    const { endTime, } = this.state;
+    // 这里表示, 如果该时间点晚于(大于)某一时间点, 则不可选
+    // 校验的current为选择框内所有日期值, 做穷举判断
+    return current > moment(endTime)
+  }
+  // 结束时间校验
+  disabledEndTime(current) {
+    const { startTime, } = this.state;
+    // 该时间点早于(小于)某一时间点
+    return current < moment(startTime)
   }
 
   // 翻页事件
@@ -125,10 +159,12 @@ class airportTransfer extends React.Component {
           <span>起始时间: </span>
           <DatePicker value={startTime}
                       onChange={(date, dateString) => this.setState({startTime: date})}
+                      disabledDate={this.disabledStartTime.bind(this)}
           />
           <span style={{marginLeft: 10}}>结束时间: </span>
           <DatePicker value={endTime}
                       onChange={(date, dateString) => this.setState({endTime: date})}
+                      disabledDate={this.disabledEndTime.bind(this)}
           />
           <Button type="primary"
                   style={{marginLeft: 10}}
